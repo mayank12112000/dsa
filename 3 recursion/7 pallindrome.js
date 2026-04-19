63e7d7105765bdf6982e4f423e2d980aeee08906
Mayank  Keshari <mayank.keshari.93@gmail.com>
2026-04-19 23:50:04 +0530

    Update 7 pallindrome.js

--- 3 recursion/7 pallindrome.js
+++ 3 recursion/7 pallindrome.js
@@ -8,6 +8,12 @@ Input: Str = “TAKE U FORWARD”
 Output: Not Palindrome
 Explanation: String when reversed is not the same as string.*/
 
-let str = "mayak"
+let str = "abccba"
 
-console.log(str[2])
+const isPalindrome =(str,i)=>{
+    if(i>=str.length/2) return true
+    if(str[i] !== str[(str.length)-1-i]) return false
+    return isPalindrome(str,i+1)
+}
+
+console.log(isPalindrome(str,0))
