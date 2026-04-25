5cb32a7f5e74c2d9169aa58146dc0227f2caff57
Mayank  Keshari <mayank.keshari.93@gmail.com>
2026-04-25 11:39:37 +0530

    Update 4 remove duplicated from sorted array.js

--- 6 arrays/4 remove duplicated from sorted array.js
+++ 6 arrays/4 remove duplicated from sorted array.js
@@ -5,8 +5,36 @@ Input: arr[]=[1,1,1,2,2,3,3,3,3,4,4]
 Output: [1,2,3,4,_,_,_,_,_,_,_]
 Explanation: Total number of unique elements are 4, i.e[1,2,3,4] and Therefore return 4 after assigning [1,2,3,4] in the beginning of the array. */
 const arr=[1,1,1,2,2,3,3,3,3,4,4]
-
+// brute force
+/*
 const removeDuplicate = (arr)=>{
-    let n = arr.length
-    
+    let index= 0
+    let seen = new Set()
+
+    for(let num of arr){
+        if(!seen.has(num)){
+            seen.add(num)
+            arr[index++] = num
+        }
+    }
+    return arr.slice(0,index)
 }
+
+console.log([...removeDuplicate(arr)])
+*/
+const removeDuplicateTwoPointer=(arr)=>{
+    let n= arr.length;
+    if(n<2) return arr
+    let i = 0; let j =1
+    while(j<n){
+        if(arr[j]!==arr[i]){
+            arr[i+1] = arr[j]
+            i++
+        }
+        j++
+    } 
+    return i+1
+}
+
+let k = removeDuplicateTwoPointer(arr)
+console.log(arr.slice(0,k))
